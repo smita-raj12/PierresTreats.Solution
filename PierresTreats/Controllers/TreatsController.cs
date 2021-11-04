acde97821fc0c0092a95c00dd9f7015baad268a9
smita Raj <smita.raj12@gmail.com>
2021-11-04 16:13:33 -0700

    add adding extra flavors to treat functionility

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -73,6 +73,24 @@ namespace PierresTreats.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
     }
+    public ActionResult AddFlavor(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+            return View(thisTreat);
+        }
+
+        [HttpPost]
+        public ActionResult AddTreat(Treat treat, int FlavorId)
+        {
+            if (FlavorId != 0)
+            {
+                _db.TreatFlavor.Add(new TreatFlavor()
+                    { FlavorId = FlavorId, TreatId = treat.TreatId });
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     public ActionResult Delete(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
