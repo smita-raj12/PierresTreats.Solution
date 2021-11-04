1b52b46ad7af9d97523e5390b05c95b97daea920
smita Raj <smita.raj12@gmail.com>
2021-11-04 14:27:18 -0700

    add read functionality in treat controller

---
+++ PierresTreats/Controllers/TreatsController.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Treatization;
+
+namespace PierresTreats.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+
+    public TreatsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+     [AllowAnonymous]
+    public ActionResult Index()
+    {
+      IQueryable<Treat> userTreats = _db.Treats.OrderBy(nameof => name.Name);
+      if (!string.IsNullOrEmpty(searchString))
+            {
+                userTreats = userTreats
+                        .Where(name => name.Name.Contains(searchString));
+            }
+      return View(userTreats.ToList());
+    }
+  }
+}    
