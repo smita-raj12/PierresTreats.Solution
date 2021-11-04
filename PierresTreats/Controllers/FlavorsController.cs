ae8acc52937457bcce1608021170a560261a80b9
smita Raj <smita.raj12@gmail.com>
2021-11-04 14:01:31 -0700

    add delete functionality in flavor controlller

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -112,5 +112,33 @@ namespace PierresTreats.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult Delete(int id)
+        {
+            var thisFlavor =
+                _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+            return View(thisFlavor);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisFlavor =
+                _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+            _db.Flavors.Remove (thisFlavor);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteTreat(int joinId)
+        {
+            var joinEntry =
+                _db
+                    .TreatFlavor
+                    .FirstOrDefault(entry => entry.TreatFlavorId == joinId);
+            _db.TreatFlavor.Remove (joinEntry);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }        
